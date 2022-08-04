b480833f60a4aafb310f42025b25c1729178a58d
Abner <107959746+Abnernp@users.noreply.github.com>
2022-08-04 10:42:37 -0300

    Update exer5_25.py

--- exer5_25.py
+++ exer5_25.py
@@ -1,7 +1,7 @@
 #5.25
 n = int(input("Digite um número: "))
 b = 2
-while (n - (b * b)) > 0.00001:
+while abs(n - (b * b)) > 0.00001:
     p = (b + (n / b)) / 2
     b = p
 print(f"A raiz quadrada de {n} é aproximadamente {p:4.4f}")
