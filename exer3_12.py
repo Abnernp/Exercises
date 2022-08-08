3a3e373b5c5ee657548d2f811ed02b5908380bfe
Abner de Paula <abnernp@yahoo.com.br>
2022-08-08 10:13:12 -0300

    rename

--- exer3_12.py
+++ exer3_12.py
@@ -1,6 +1,6 @@
 # Cálculo do tempo de percurso
 
-distancia = float(input("Digite a distância em km: "))
+distancia = float(input("Digite a distância em km(s: "))
 velocidade = float(input("Digite a velocidade média em km/h: "))
 tempo = distancia / velocidade
 print("O tempo médio da viagem será de :", tempo, "horas")
